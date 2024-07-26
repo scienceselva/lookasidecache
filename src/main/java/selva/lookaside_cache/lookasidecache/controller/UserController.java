69ba436c168110620a7cf42b87ee30feace28e9f
scienceselva <scienceselva@gmail.com>
2024-07-26 20:14:17 +0530

    update with cache endpoint

--- src/main/java/selva/lookaside_cache/lookasidecache/controller/UserController.java
+++ src/main/java/selva/lookaside_cache/lookasidecache/controller/UserController.java
@@ -1,10 +1,16 @@
 package selva.lookaside_cache.lookasidecache.controller;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.web.bind.annotation.*;
 import selva.lookaside_cache.lookasidecache.model.User;
 import selva.lookaside_cache.lookasidecache.service.UserService;
 
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -15,8 +21,11 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
-    public Optional<User> getUserById(@PathVariable Long id) {
-        return userService.getUserById(id);
+    public Optional<User> getUserById(
+            @PathVariable Long id,
+            @RequestParam(required = false) String name
+    ) {
+        return userService.getUserById(id, name);
     }
 
     @GetMapping("/test-cache")
@@ -24,6 +33,24 @@ public class UserController {
         // This is just a test endpoint to check caching
         return "Cache test endpoint";
     }
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @GetMapping("/cache/users")
+    public List<String> getCacheContents() {
+        Cache<Object, Object> cache = cacheManager.getCache("users");
+
+        List<String> cacheContents = new ArrayList<>();
+        if (cache != null) {
+            cache.forEach(entry -> cacheContents.add("Key: " + entry.getKey() + ", Value: " + entry.getValue()));
+        } else {
+            cacheContents.add("Cache 'users' is not available.");
+        }
+        return cacheContents;
+    }
+
+
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.saveUser(user);
--- src/main/java/selva/lookaside_cache/lookasidecache/service/UserService.java
+++ src/main/java/selva/lookaside_cache/lookasidecache/service/UserService.java
@@ -1,11 +1,15 @@
 package selva.lookaside_cache.lookasidecache.service;
 
+import jakarta.annotation.PostConstruct;
 import selva.lookaside_cache.lookasidecache.model.User;
 import selva.lookaside_cache.lookasidecache.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
 import java.util.Optional;
 
 @Service
@@ -14,9 +18,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Cacheable(value = "users", key = "#id")
-    public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
+    @Cacheable(value = "users", key = "#id + ':' + #name")
+    public Optional<User> getUserById(Long id, String name) {
+
+        if(name == null) {
+            return userRepository.findById(id);
+        }else{
+            return userRepository.findByIdAndName(id, name);
+        }
+
     }
 
     public User saveUser(User user) {
